e3f3fbc22d65da579eb7b40e913eda97212ec9df
Ricky <ricky@dius.com.au>
2015-12-21 23:27:34 +1100

    made a faster way to add new results

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,7 +1,5 @@
 class RatingsController < ApplicationController
   def index
-    Rails.cache.fetch("game", expires_in: 1.hour) do
-      @game = Game.find(params[:game_id])
-    end
+    @game = Game.find(params[:game_id])
   end
 end
