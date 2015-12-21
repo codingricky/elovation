34b7b7f01f297901ba222a418c56525554e46c62
Ricky <ricky@dius.com.au>
2015-12-21 23:12:14 +1100

    added some caching to the ratings

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,5 +1,7 @@
 class RatingsController < ApplicationController
   def index
-    @game = Game.find(params[:game_id])
+    Rails.cache.fetch("game", expires_in: 1.hour) do
+      @game = Game.find(params[:game_id])
+    end
   end
 end
