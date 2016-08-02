c756e827f3cf9fb05f044ebf83f8b5c9241dfdc6
Daniel Deng <ddeng@dius.com.au>
2016-08-02 21:04:39 +1000

    moved chart_data to games_controller where it belongs to

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,30 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  def chart_data(game)
-    players = Player.all.includes(ratings: :history_events).where(ratings: { game: game })
-
-    player_to_days = Hash.new
-    every_day = Set.new
-    players.each do |player|
-      day_to_event = Hash.new
-      RatingHistoryEvent.events(player, game).each do |event|
-        day_to_event[event.created_at.to_date.to_s] = event.value
-        every_day.add(event.created_at.to_date.to_s)
-      end
-      player_to_days[player.name] = day_to_event
-    end
-
-    players.each do |player|
-      last_rating = nil
-      every_day.to_a.sort.each_with_index do |day, i|
-        last_rating = player_to_days[player.name].fetch(day, last_rating)
-        player_to_days[player.name][day] = last_rating
-      end
-    end
-
-    players.map do |player|
-      {:name => player.name, :data => player_to_days[player.name].to_a}
-    end
-  end
 end
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -62,4 +62,31 @@ class GamesController < ApplicationController
                                 :max_number_of_players_per_team,
                                 :allow_ties)
   end
+
+  def chart_data(game)
+    players = Player.all.includes(ratings: :history_events).where(ratings: { game: game })
+
+    player_to_days = Hash.new
+    every_day = Array.new
+    players.each do |player|
+      day_to_event = Hash.new
+      RatingHistoryEvent.events(player, game).each do |event|
+        day_to_event[event.created_at.to_date.to_s] = event.value
+        every_day << event.created_at.to_date.to_s
+      end
+      player_to_days[player.name] = day_to_event
+    end
+
+    players.each do |player|
+      last_rating = nil
+      every_day.each do |day|
+        last_rating = player_to_days[player.name].fetch(day, last_rating)
+        player_to_days[player.name][day] = last_rating
+      end
+    end
+
+    players.map do |player|
+      {:name => player.name, :data => player_to_days[player.name].to_a}
+    end
+  end
 end
