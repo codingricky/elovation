8774f580ada4b440fe395d611b7de91ed16188d4
Ricky <ricky@dius.com.au>
2017-08-09 08:39:30 +1000

    changed light colours

--- app/controllers/api/lights_controller.rb
+++ app/controllers/api/lights_controller.rb
@@ -7,9 +7,19 @@ class Api::LightsController < Api::ApiBaseController
     winner = last_result.winner
     loser = last_result.loser
     no_color = 'black'
+    winner_color = 'cyan'
+    loser_color = 'red'
     all_ratings = Game.default.all_ratings_with_active_players.collect
     all_ratings.each do |rating|
-      rating.player.color = no_color unless [winner, loser].include?(rating.player)
+      player = rating.player
+      if player == winner
+        color = winner_color
+      elsif player == loser
+        color = loser_color
+      else
+        color = no_color
+      end
+      player.color = color
     end
     render json: all_ratings
   end
